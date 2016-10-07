2820f3fe7472e3bd632585c1340fc5266f1656e0
d0f <284178359@qq.com>
2016-10-07 19:11:04 +0800

    thinkphp 3.2 framework base common

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+define('APP_NAME','App');
+define('APP_PATH','./App/');
+define('THINK_PATH','./ThinkPHP/');
+define('ENGINE_NAME','cluster');
+define('APP_DEBUG',true);
+define("TOKEN", "hcwx");
+define("ROOT_PATH",dirname(__FILE__));
+require THINK_PATH.'ThinkPHP.php';
